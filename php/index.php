d5b8f4902e107aa1b7191af6eda494c86f823283
francicsco manuel <fran319@correo.ugr.es>
2014-01-13 17:24:22 +0100

    añadida aplicacion

---
+++ php/index.php
@@ -0,0 +1,214 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+	<title>Practica1</title>
+    <meta charset="UTF-8"/>
+    <link rel="stylesheet" type="text/css" href="ejercicioTablas.css" />		
+</head>
+
+<body background="fondo.jpg" style="background-attachment: fixed">
+
+	<h1> PRÁCTICA 1 </h1>
+    <h2> Infraestructura Virtual </h2>
+    
+   <section class="a">
+    	<img src="yo.jpg">
+    </section>
+    	
+    <section class="a">
+    	<p> Alumno: Francisco Manuel Sánchez Ramos </p>
+        <p> Correo: <a href="mailto:fran319@correo.ugr.es"> fran319@correo.ugr.es </a> </p>
+        <p> twitter: <a href="https://twitter.com/fran_manu90"> @fran_manu90 </a></p>
+        <p> Grado en Ingeniería Informática (Plan 2010) </p>
+        <br>
+        <br>
+     
+    </section>
+
+	<table class="borde" border="1" align="center" summary="Esta tabla muestra mi horario de clase">
+    	<caption> Mi horario </caption>
+        <thead>
+        	<tr>
+            	<th class="a" rowspan="5"> Horas </th>
+                <th class="a" colspan="5"> Dias </th>
+            </tr>
+            <tr>
+            	<th class="b"> Lunes </th>
+                <th class="b"> Martes </th>
+                <th class="b"> Miércoles </th>
+                <th class="b"> Jueves </th>
+                <th class="b"> Viernes </th>
+            </tr>
+        </thead>
+        
+        <tbody>
+        	<tr>
+            	<th class="c"> 08:30-09:30 </th>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+ 				<th class="c"> 09:30-10:30 </th>
+                <td class="b"> DAI - Prácticas </td>
+                <td class="c"> </td>
+                <td class="b"> SPSI - Prácticas </td>
+                <td class="c"> </td>
+                <td class="b"> IV - Prácticas </td>
+            </tr>
+            <tr>
+                <th class="c"> 10:30-11:30 </th>
+                <td class="b"> DAI - Prácticas </td>
+                <td class="c"> </td>
+                <td class="b"> SPSI - Prácticas </td>
+                <td class="c"> </td>
+                <td class="b"> IV - Prácticas </td>
+            </tr>
+            <tr>
+                <th class="c"> 11:30-12:30 </th>
+                <td class="a"> IV - Teoría </td>
+                <td class="c"> </td>
+                <td class="a"> DAI - Teoría </td>
+                <td class="c"> </td>
+                <td class="a"> SPSI - Teoría </td>
+            </tr>
+            <tr>
+                <th class="c"> 12:30-13:30 </th>
+                <td class="a"> IV - Teoría </td>
+                <td class="c"> </td>
+                <td class="a"> DAI - Teoría </td>
+                <td class="c"> </td>
+                <td class="a"> SPSI - Teoría </td>
+            </tr>
+            <tr>
+                <th class="c"> 13:30.14:30 </th>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+                <th class="c"> 14:30-15:30 </th>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>   
+                <th class="c"> 15:30-16:30 </th>
+                <td class="a"> IG - Teoría </td>
+                <td class="a"> FR - Teoría </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+                <th class="c"> 16:30-17:30 </th>
+                <td class="a"> IG - Teoría </td>
+                <td class="a"> FR - Teoría </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+                <th class="c"> 17:30-18:30 </th>
+                <td class="c"> </td>
+                <td class="b"> IG - Prácticas </td>
+                <td class="b"> FR - Prácticas </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>     
+                <th class="c"> 18:30-19:30 </th>
+                <td class="c"> </td>
+                <td class="b"> IG - Prácticas </td>
+                <td class="b"> FR - Prácticas </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+                <th class="c"> 19:30-20:30 </th>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+            <tr>
+                <th class="c"> 20:30-21:30 </th>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+                <td class="c"> </td>
+            </tr>
+        </tbody>
+    </table>
+    
+    <h2> Mis asignaturas. Curso 2013-2014 </h2>
+    
+    <section>    
+        <article>
+            <h3> Curso 1º. 1º Cuatrimestre </h3>
+            <table class="borde" border="1" align="center" summary="Tabla con las asignaturas de primero del primer cuatrimestre">
+            <thead>
+            	<tr>
+                	<th rowspan="4" class="d"> Código </th>
+                </tr>
+                <tr>
+                	<th class="d"> Asignaturas </th>
+                    <th class="d"> ECTS </th>
+                    <th class="d"> Carácter </th>
+                    <th class="d"> Curso </th>
+                </tr>
+            </thead>
+            
+            <tbody>
+            	<tr>
+                	<th class="d"> 40101102 </th>
+                    <td class="c"> Informática Gráfica </a> </td>
+                    <td class="c"> 6 </td>
+                    <td class="c"> Obligatoria </td>
+                    <td class="c"> 3º </td>
+                </tr>
+                <tr>
+                	<th class="d"> 40101104 </th>
+                    <td class="c"> Fundamentos de Redes </a> </td>
+                    <td class="c"> 6 </td>
+                    <td class="c"> Obligatoria </td>
+                    <td class="c"> 3º </td>
+                </tr>
+                <tr>
+                	<th class="d"> 40101106 </th>
+                    <td class="c"> Infraestructura Virtual </a> </td>
+                    <td class="c"> 6 </td>
+                    <td class="c"> Obligatoria </td>
+                    <td class="c"> 4º TI </td>
+                </tr>
+                <tr>
+                	<th class="d"> 40101101 </th>
+                    <td class="c"> Diseño de Aplicaciones para Internet </a> </td>
+                    <td class="c"> 6 </td>
+                    <td class="c"> Obligatoria </td>
+                    <td class="c"> 4º TI </td>
+                </tr>
+                <tr>
+                	<th class="d"> 40101109 </th>
+                    <td class="c"> Seguridad y Protección de Sistemas Informáticos </a> </td>
+                    <td class="c"> 6 </td>
+                    <td class="c"> Obligatoria </td>
+                    <td class="c"> 4º TI </td>
+                </tr>
+            </tbody>
+            </table>
+                   
+        </article>
+    </section>  
+
+</body>
+</html>
